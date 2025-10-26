23d3375072e9c1410f76670f31b0c35ef199e7b0
lewys-miugo <lewiswambugu01@gmail.com>
2025-10-26 23:45:41 +0300

    image backdrop

--- frontend/components/Authentication/AuthPage.jsx
+++ frontend/components/Authentication/AuthPage.jsx
@@ -12,8 +12,13 @@ export default function AuthPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="w-full max-w-md">
+    <div  className="min-h-screen bg-cover bg-center flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 relative"
+      style={{ backgroundImage: "url('./solar_bg.jpg')" }}>
+      {/* Optional overlay for readability */}
+      <div className="absolute inset-0 bg-black/40"></div>
+
+      {/* Foreground form content */}
+      <div className="relative w-full max-w-md backdrop-blur-xs rounded-xl shadow-lg p-2">
         {isLogin ? (
           <LoginForm onToggle={toggleForm} />
         ) : (
