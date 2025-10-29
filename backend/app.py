9e5137dd3a82dcf9ce891f5fca349634647c5ad2
lewys-miugo <lewiswambugu01@gmail.com>
2025-10-30 02:17:25 +0300

    Prepping app for deployment

--- backend/app.py
+++ backend/app.py
@@ -317,6 +317,7 @@ def api_info():
     }), 200
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=port)
